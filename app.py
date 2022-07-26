be772af302e2186255c9d797e0e7112beb7a4226
Sasa537 <sasa5375632@gmail.com>
2022-07-26 21:32:15 +0300

    Исправил замечания Vitaly-GR

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 from users import db_user
 
 app = Flask(__name__)
-db_user.init_app(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = 'a123456789'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+db_user.init_app(app)
 
 
 @app.route("/")
@@ -17,6 +18,12 @@ def index():
 def login():
 
     return render_template('login.html')
+
+
+@app.route("/registr")
+def reg():
+
+    return render_template('registr.html')
 
 
 if __name__ == "__main__":
