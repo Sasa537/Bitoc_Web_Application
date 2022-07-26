ab7e7e9f5f2dcc2d639ea9e22b359b0b13b489fe
Sasa537 <sasa5375632@gmail.com>
2022-07-26 18:04:44 +0300

    Добавлен весь фронт и модель пользователя

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from users import db_user
 
 app = Flask(__name__)
+db_user.init_app(app)
+app.config['SECRET_KEY'] = 'a123456789'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 
 
 @app.route("/")
 def index():
+    db_user.create_all()
+    return render_template('index.html')
 
-    return "This is index page our web application"
+
+@app.route("/login")
+def login():
+
+    return render_template('login.html')
 
 
 if __name__ == "__main__":
