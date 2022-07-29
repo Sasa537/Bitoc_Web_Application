ea902d8d7c344c1eb1b7bffa41154c4e3e969026
Sasa537 <sasa5375632@gmail.com>
2022-07-29 15:55:36 +0300

    Реализована регистрация пользователей

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
-from users import db_user
+from flask import Flask, render_template, redirect, request
+from users import db_user, User, login
+from flask_login import current_user
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = 'a123456789'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 db_user.init_app(app)
+login.init_app(app)
 
 
 @app.route("/")
@@ -20,10 +22,20 @@ def login():
     return render_template('login.html')
 
 
-@app.route("/registr")
+@app.route("/register", methods=['POST', 'GET'])
 def reg():
+    if request.method == 'POST':
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
 
-    return render_template('registr.html')
+        user = User(email=email, username=username)
+        user.set_password(password)
+        db_user.session.add(user)
+        db_user.session.commit()
+        return redirect('/login')
+    else:
+      return render_template('register.html')
 
 
 if __name__ == "__main__":
